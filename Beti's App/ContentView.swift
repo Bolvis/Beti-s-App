a6ed73b3dc8b08bceddb247b04133ca766333589
Dawid <dawid.stasiak17@gmail.com>
2020-05-29 17:01:23 +0200

    Initial Commit

---
+++ Beti's App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Beti's App
+//
+//  Created by Dawid Stasiak on 29/05/2020.
+//  Copyright © 2020 Dawid Stasiak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
